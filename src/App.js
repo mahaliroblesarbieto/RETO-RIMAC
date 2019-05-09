9dba0bfdc81f7f69ff4df9de671b73921ef3e538
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-05-09 18:58:01 -0500

    añadiendo header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
-import CarouselPrincipal from './components/CarouselPrincipal'
+import Principal from './pages/Principal';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <CarouselPrincipal />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Principal/> 
       </header>
     </div>
   );
---
+++ src/pages/Principal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavPrincipal from '../components/NavPrincipal';
+
+export default function Principal() {
+
+    return (
+      <div>
+        <div className="row">
+          <NavPrincipal/>
+        </div>
+      </div>
+    );
+}

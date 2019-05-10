39c85ff975ab5642e56869cf0c80eb77ae1f6250
GiulianaMS <pamelita.suma@gmail.com>
2019-05-10 04:20:52 -0500

    Se actualiza cambios

--- src/components/CarouselPrincipal.jsx
+++ src/components/CarouselPrincipal.jsx
@@ -8,20 +8,6 @@ import Col from 'react-bootstrap/Col';
 
 const CarouselPrincipal = () => (
 	<Carousel className="width">
-		<Carousel.Item className="carousel-item1">
-			<Container className="carousel-page-one">
-				<Row>
-					<Col sm={5}>
-						<h1>Protege tu hogar desde <span className="precio-text">S/ 14.90 </span>al mes</h1>
-						<p>¡Toc, toc! Estés o no en casa, tu tranquilidad está asegurada, y al mejor precio</p>
-						<button className="carousel-btn">SOLICÍTALO AQUI</button>
-					</Col>
-					<Col sm={6}>
-						<img className="d-block w-100 carousel-img" src="https://user-images.githubusercontent.com/45071050/57510696-0e94b680-72cd-11e9-8a84-298fd7e1125e.png" alt=""/>
-					</Col>
-				</Row>
-			</Container>
-		</Carousel.Item>
 		<Carousel.Item className="carousel-item2">
 			<Container className="carousel-page-one">
 				<Row>
@@ -32,6 +18,20 @@ const CarouselPrincipal = () => (
 					</Col>
 					<Col sm={6}>
 						<img className="d-block w-100 carousel-img" src="https://user-images.githubusercontent.com/45071050/57514100-4b64ab80-72d5-11e9-9a8a-252e574d77dc.png" alt=""/>
+					</Col>
+				</Row>
+			</Container>
+		</Carousel.Item>
+		<Carousel.Item className="carousel-item1">
+			<Container className="carousel-page-one">
+				<Row>
+					<Col>
+						<h1>Te llevamos a disfrutar la final de la copa Brasil</h1>
+						<p>Gana un paquete doble sin sorteos y más.</p>
+						<button className="carousel-btn">CONOCE MÁS</button>
+					</Col>
+					<Col>
+						<img className="d-block w-100 carousel-img" src="https://user-images.githubusercontent.com/45071050/57488063-a2d42e80-7277-11e9-801d-ea0932d7d23a.png" alt=""/>
 					</Col>
 				</Row>
 			</Container>

e9419c7a41c000e4e039ee181753bf3e21e9f68d
GloryRojas <grojasm94@gmail.com>
2019-05-09 19:27:57 -0500

    Mostrando beneficios

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import './Header.scss';
 
 const Header = () => {
 	return (
 		<div>
-				<h5><Link to='/'>Volver</Link></h5>
+			<h5><Link to='/'>Volver</Link></h5>
 		</div>
 	)
 };

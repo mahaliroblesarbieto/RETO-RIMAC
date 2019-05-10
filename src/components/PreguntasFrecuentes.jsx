907a9e61f03c211f5adbbda96f89fad6fd6e9e23
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-05-10 02:11:03 -0500

    cambiando de tamaño p

--- src/components/PreguntasFrecuentes.jsx
+++ src/components/PreguntasFrecuentes.jsx
@@ -19,19 +19,19 @@ const PreguntasFrecuentes = () => {
         <h4>¿Qué es un seguro Hogar a tu medida?</h4>
       </div>
       <div class="row">
-        <p>Este seguro te permite proteger tu casa y su contenido ante <br/> cualquier incidente, y al mejor precio. Según el plan que  <br/> contrates, este tipo de seguro protege tu hogar ante sismos, <br/> incendios, robos y más.</p>
+        <p class="black">Este seguro te permite proteger tu casa y su contenido ante <br/> cualquier incidente, y al mejor precio. Según el plan que  <br/> contrates, este tipo de seguro protege tu hogar ante sismos, <br/> incendios, robos y más.</p>
       </div>
       <div class="row">
         <h4>¿Por qué proteger mi casa?</h4>
       </div>
       <div class="row">
-        <p>Tu casa propia es el resultado de tu esfuerzo y siempre es <br/> mejor que ese esfuerzo esté protegido y que, en caso de <br/> siniestro, podamos ayudarte a recuperarla sin necesidad de  <br/> grandes inversiones.</p>
+        <p class="black">Tu casa propia es el resultado de tu esfuerzo y siempre es <br/> mejor que ese esfuerzo esté protegido y que, en caso de <br/> siniestro, podamos ayudarte a recuperarla sin necesidad de  <br/> grandes inversiones.</p>
       </div>
       <div class="row">
         <h4>¿Cómo comprar un seguro Hogar?</h4>
       </div>
       <div class="row">
-        <p>Elige el plan que más te convenga y solicítalo desde esta <br/> misma web o acércate a cualquiera de nuestros centros de <br/> atención RIMAC a nivel nacional.  </p>
+        <p class="black">Elige el plan que más te convenga y solicítalo desde esta <br/> misma web o acércate a cualquiera de nuestros centros de <br/> atención RIMAC a nivel nacional.  </p>
       </div>
 
     </div>

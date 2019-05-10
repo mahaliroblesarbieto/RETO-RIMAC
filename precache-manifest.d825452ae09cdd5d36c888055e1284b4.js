dcdc0e48cb05eb555fe35ab51c923b0a269af6c7
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-05-10 02:37:18 -0500

    Updates

---
+++ precache-manifest.d825452ae09cdd5d36c888055e1284b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c3d9b5d314f5da9e6bd0d837208025",
+    "url": "/RETO-RIMAC/index.html"
+  },
+  {
+    "revision": "97f3db345f4e71293aae",
+    "url": "/RETO-RIMAC/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "95bf426dbe6ab893e721",
+    "url": "/RETO-RIMAC/static/css/main.e8d62151.chunk.css"
+  },
+  {
+    "revision": "97f3db345f4e71293aae",
+    "url": "/RETO-RIMAC/static/js/2.01f5c3be.chunk.js"
+  },
+  {
+    "revision": "95bf426dbe6ab893e721",
+    "url": "/RETO-RIMAC/static/js/main.c7784e1a.chunk.js"
+  },
+  {
+    "revision": "d5ff11e730f380785006",
+    "url": "/RETO-RIMAC/static/js/runtime~main.bbf0f5be.js"
+  }
+]);
--- src/pages/Pdp.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Pdp = () => (<div>Soy el pdp</div>);
-
-export default Pdp;
